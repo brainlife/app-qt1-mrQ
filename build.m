97a834f3fc75febb5fc2eadfd35cd0cc0231881d
Bradley Caron <bacaron@i4.red.uits.iu.edu>
2021-02-24 18:29:14 -0500

    reason: recompile to add binary

--- build.m
+++ build.m
@@ -5,5 +5,5 @@ addpath(genpath('/N/soft/mason/SPM/spm8'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/project/plab/bacaron/git/mrQ'))
 addpath(genpath('/N/project/plab/bacaron/git/knkutils'))
-mcc -m -R -nodisplay -d compiled mrQBrainlife
+mcc -m -R -nodisplay -d compiled -a /N/u/brlife/git/vistasoft/mrAnatomy/Segment/bet2 mrQBrainlife
 exit
