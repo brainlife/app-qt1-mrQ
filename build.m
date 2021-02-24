4137dbe37c88c2e8d973e0f83158c784083da8d8
Bradley Caron <bacaron@i4.red.uits.iu.edu>
2021-02-24 17:32:11 -0500

    reason: compile

---
+++ build.m
@@ -0,0 +1,8 @@
+addpath(genpath('/N/soft/rhel7/ants/2.3.1/bin/'))
+addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/soft/mason/SPM/spm8'))
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/N/project/plab/bacaron/git/mrQ'))
+addpath(genpath('/N/project/plab/bacaron/git/knkutils'))
+mcc -m -R -nodisplay -d compiled mrQBrainlife
+exit
--- compile.sh
+++ compile.sh
@@ -4,12 +4,13 @@ module load matlab/2017a
 mkdir -p compiled
 
 cat > build.m <<END
+addpath(genpath('/N/soft/rhel7/ants/2.3.1/bin/'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/project/plab/bacaron/git/mrQ'))
 addpath(genpath('/N/project/plab/bacaron/git/knkutils'))
-mcc -m -R -nodisplay -d compiled main
+mcc -m -R -nodisplay -d compiled mrQBrainlife
 exit
 END
 matlab -nodisplay -nosplash -r build
