c1ebe4865aac5d10d678ae2e89e4e1440a0a8065
Bradley Caron <bacaron@i4.red.uits.iu.edu>
2021-02-24 18:04:12 -0500

    reason: recompile

--- build.m
+++ build.m
@@ -1,3 +1,4 @@
+addpath(genpath('/N/soft/rhel7/fsl/5.0.11b/bin/'))
 addpath(genpath('/N/soft/rhel7/ants/2.3.1/bin/'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ module load matlab/2017a
 mkdir -p compiled
 
 cat > build.m <<END
+addpath(genpath('/N/soft/rhel7/fsl/5.0.11b/bin/'))
 addpath(genpath('/N/soft/rhel7/ants/2.3.1/bin/'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
